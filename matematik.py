7ca04e57a12ded464c48f636c6b167e8edf2327a
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-03 02:31:58 +0300

    Kalıtım işlendi.

--- matematik.py
+++ matematik.py
@@ -17,3 +17,15 @@ matematik=Matematik(8,4)
 sonuc=matematik.bol()
 
 print("somuc : " + str(sonuc))
+
+
+class Istatistik(Matematik):
+    def __init__(self,sayi1,sayi2):
+        super().__init__(sayi1,sayi2)
+    def varyansHesapla (self):
+        return self.s1*self.s2 
+
+
+
+istatistik =Istatistik(5,8)
+
--- workshop2.py
+++ workshop2.py
@@ -1,12 +1,11 @@
 faktoriyel=int(input("Faktoriyeli hesapalanacak sayıyı giriniz : "))
-toplam=0
+toplam=1
 
 if  faktoriyel<0:
     print("0 dan küçük olamaz")
 elif  faktoriyel ==0:
-    toplam=1
-    print("faktoriyel = : "+ str(faktoriyel))
+    print("faktoriyel = : "+ str(toplam))
 else:
     for fak in range(faktoriyel,0,-1):
-        toplam=toplam+(faktoriyel*fak-1)
-print(toplam)        
+        toplam=fak*toplam
+    print(toplam)        
