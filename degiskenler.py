10114f41fd49b970e5a3a300a85287b778335fe8
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-02 22:36:41 +0300

    Commit

--- degiskenler.py
+++ degiskenler.py
@@ -1,1 +1,13 @@
-print("sa")
+isim="Kubilay Yazi"
+sayi =10
+floatSayi=10.5
+tc= "1111111111"
+
+durum=False
+
+print(type(floatSayi))
+print(type(sayi))
+print(type(tc))
+print(type(isim))
+print(type(durum))
+
