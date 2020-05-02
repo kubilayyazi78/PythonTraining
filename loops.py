6ceed7cf8a83ae8d87a51fa2da4fb76cdebe7f86
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-03 01:12:49 +0300

    Dönüler tamamlandı.

---
+++ loops.py
@@ -0,0 +1,22 @@
+sehirler =["Karabük","Makedonya","Erzurum"]
+
+for sehir in sehirler:
+    print(sehir)
+
+
+
+for sayi in range(1,10,2):
+    print(sayi)
+
+
+sayac=1
+while sayac<=10:
+    print(sayac)
+    sayac=sayac+1
+
+
+isim=input("Adınız")
+print("isim : "+isim)
+
+sayi=int(input("Sayı : "))
+
---
+++ workshop2.py
@@ -0,0 +1,12 @@
+faktoriyel=int(input("Faktoriyeli hesapalanacak sayıyı giriniz : "))
+toplam=0
+
+if  faktoriyel<0:
+    print("0 dan küçük olamaz")
+elif  faktoriyel ==0:
+    toplam=1
+    print("faktoriyel = : "+ str(faktoriyel))
+else:
+    for fak in range(faktoriyel,0,-1):
+        toplam=toplam+(faktoriyel*faktoriyel-1)
+print(toplam)        
